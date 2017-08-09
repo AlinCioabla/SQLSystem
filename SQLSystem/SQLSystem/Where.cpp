ddaed0a8f57847be616074f70def800af55f84ac
Isabela <bala.andreea@yahoo.com>
2017-08-09 10:11:18 +0300

    no message

--- SQLSystem/SQLSystem/Where.cpp
+++ SQLSystem/SQLSystem/Where.cpp
@@ -1,16 +1,101 @@
 #include "stdafx.h"
 #include "Where.h"
+#include "From.h"
+#include "Helpers.h"
+#include "Invalid.h"
+#include "Valid.h"
 
 Where::Where()
 {
 }
 
 IState * Where::HandleToken(TokenPtr &   aCurrentToken,
-                            TokenPtr &   prevToken,
+                            TokenPtr &   aPrevToken,
                             AstNodePtr & aCurrentInstructionNode,
                             Ast &        aAst)
 {
-  return nullptr;
+  if (aCurrentToken->GetWord() == "AND" || aCurrentToken->GetWord() == "OR")
+  {
+    if (aPrevToken->GetType() == PredicateType || IsNumber(aPrevToken->GetWord()))
+    {
+      aAst.InsertLeft(aCurrentInstructionNode, aCurrentToken);
+    }
+    else
+      return new Invalid();
+  }
+
+  if (aCurrentToken->GetType() == OperatorType || aCurrentToken->GetWord() == "LIKE" ||
+      aCurrentToken->GetWord() == "NOTLIKE")
+  {
+    if (aPrevToken->GetType() == IdentifierType)
+    {
+      if (aCurrentInstructionNode->GetLeft()->GetToken()->GetWord() == "AND" ||
+          aCurrentInstructionNode->GetLeft()->GetToken()->GetWord() == "OR")
+      {
+        auto temp = aCurrentInstructionNode->GetLeft()->GetRight();
+        aCurrentInstructionNode->GetLeft()->SetRight(Ast::GetNewNode(aCurrentToken));
+        aCurrentInstructionNode->GetLeft()->GetRight()->SetLeft(temp);
+      }
+      else
+      {
+        aAst.InsertLeft(aCurrentInstructionNode, aCurrentToken);
+      }
+    }
+    else
+    {
+      return new Invalid();
+    }
+  }
+
+  if (aCurrentToken->GetType() == IdentifierType)
+  {
+    if (aPrevToken->GetType() == KeywordType)
+    {
+      aAst.InsertLeft(aCurrentInstructionNode, aCurrentToken);
+    }
+    else if (aPrevToken->GetWord() == "AND" || aPrevToken->GetWord() == "OR")
+    {
+      aAst.InsertRight(aCurrentInstructionNode->GetLeft(), aCurrentToken);
+    }
+    else
+    {
+      return new Invalid();
+    }
+  }
+
+  if (aCurrentToken->GetType() == PredicateType || IsNumber(aCurrentToken->GetWord()))
+  {
+    if (aPrevToken->GetType() == OperatorType || aPrevToken->GetWord() == "LIKE" ||
+        aPrevToken->GetWord() == "NOTLIKE")
+    {
+      if (aCurrentInstructionNode->GetLeft()->GetToken()->GetWord() == "AND" ||
+          aCurrentInstructionNode->GetLeft()->GetToken()->GetWord() == "OR")
+      {
+        aCurrentInstructionNode->GetLeft()->GetRight()->SetRight(Ast::GetNewNode(aCurrentToken));
+      }
+      else
+      {
+        aAst.InsertRight(aCurrentInstructionNode->GetLeft(), aCurrentToken);
+      }
+    }
+    else
+      return new Invalid();
+  }
+
+  //////////////////////////////////////////////////
+  if (aCurrentToken->GetWord() == ";")
+  {
+    if (aPrevToken->GetType() == IdentifierType || aPrevToken->GetType() == PredicateType)
+    {
+      return new Valid();
+    }
+    else
+      return new Invalid();
+  }
+  else
+  {
+    return new Invalid();
+  }
 }
 
 Where::~Where()
