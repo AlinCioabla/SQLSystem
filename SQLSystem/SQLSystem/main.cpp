11c783767809badc82776fb434b07b00c1086968
AlinCioabla <alin.cioabla@caphyon.com>
2017-08-16 14:20:46 +0300

    no message

--- SQLSystem/SQLSystem/main.cpp
+++ SQLSystem/SQLSystem/main.cpp
@@ -13,21 +13,16 @@
 void TestParser()
 {
   ParserTestUnit parserTestUnit;
-  parserTestUnit.CreateTest("TestFile1.txt"s);
-  parserTestUnit.CreateTest("TestFile2.txt"s);
-  parserTestUnit.CreateTest("TestFile3.txt"s);
-  parserTestUnit.CreateTest("TestFile4.txt"s);
-  parserTestUnit.CreateTest("TestFile5.txt"s);
-  parserTestUnit.CreateTest("TestFile6.txt"s);
-  parserTestUnit.CreateTest("TestFile7.txt"s);
-  parserTestUnit.CreateTest("TestFile8.txt"s);
-  parserTestUnit.CreateTest("TestFile9.txt"s);
-  parserTestUnit.CreateTest("TestFile10.txt"s);
   parserTestUnit.CreateTest("TestFile11.txt"s);
   parserTestUnit.CreateTest("TestFile12.txt"s);
   parserTestUnit.CreateTest("TestFile13.txt"s);
   parserTestUnit.CreateTest("TestFile14.txt"s);
   parserTestUnit.CreateTest("TestFile15.txt"s);
+  parserTestUnit.CreateTest("TestFile16.txt"s);
+  parserTestUnit.CreateTest("TestFile17.txt"s);
+  parserTestUnit.CreateTest("TestFile18.txt"s);
+  parserTestUnit.CreateTest("TestFile19.txt"s);
+  parserTestUnit.CreateTest("TestFile20.txt"s);
   parserTestUnit.ExecuteAllTests();
 }
 
@@ -51,7 +46,7 @@ void TestLexer()
 int main()
 {
   TestLexer();
-  // TestParser();
+  TestParser();
 
   return 0;
 }
