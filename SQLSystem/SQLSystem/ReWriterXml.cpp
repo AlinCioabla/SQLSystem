6e28707645a3c1f1100935be3b6e230accf868cf
Isabela <bala.andreea@yahoo.com>
2017-08-23 16:59:04 +0300

    no message

--- SQLSystem/SQLSystem/ReWriterXml.cpp
+++ SQLSystem/SQLSystem/ReWriterXml.cpp
@@ -1,2 +1,9 @@
 #include "stdafx.h"
 #include "ReWriterXml.h"
+
+vector<string> smth = {
+  "SELECT", "FROM",      "ALL",    "COLUMN", "TABLE",      "WHERE",
+  "DELETE", "INSERT",    "INTO",   "VALUES", "OR",         "AND",
+  "LIKE",   "NOTLIKE",   "EQUALS", "MINUS",  "DIVISIONOP", "MULTIPLICATIONOP",
+  "NUMBER", "PREDICATE", "COMA",
+};
