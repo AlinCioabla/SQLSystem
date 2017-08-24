60ebf0290cb9fb1f3d96fe1997dd47be6a7f19f8
Isabela <bala.andreea@yahoo.com>
2017-08-24 15:02:58 +0300

    no message

--- SQLSystem/SQLSystem/ReWriterXml.cpp
+++ SQLSystem/SQLSystem/ReWriterXml.cpp
@@ -2,19 +2,30 @@
 #include "ReWriterXml.h"
 #include "AstNode.h"
 
-vector<string> tag = { "SELECT",   " DISTINCT ", "UPDATE",  " FROM ",     " ALL ",
-                       " COLUMN ", " TABLE ",    " WHERE ", "DELETE",     "INSERT",
-                       "INTO",     "VALUES",     "OR",      "AND",        "LIKE",
-                       "NOTLIKE",  "EQUALS",     "MINUS",   "DIVISIONOP", "MULTIPLICATIONOP",
-                       "NUMBER",   "PREDICATE",  "COMA" };
+vector<string> tag = { "SELECT",     " DISTINCT ",       "UPDATE",  " FROM ",    " ALL ",
+                       " COLUMN ",   " TABLE ",          " WHERE ", "DELETE",    "INSERT",
+                       "INTO",       "VALUES",           "VALUE",   "OR",        "AND",
+                       "LIKE",       "NOTLIKE",          "EQUALS",  "PLUSOP",    "MINUS",
+                       "DIVISIONOP", "MULTIPLICATIONOP", "NUMBER",  "PREDICATE", "COMA",
+                       "INVALID" };
+
+vector<string> type = {
+  "KeywordType",    "OperatorType", "PunctuationType", "WhiteSpaceType",
+  "IdentifierType", "NumberType",   "PredicateType",   "TextualOperatorType"
+};
 
 void ReWriterXml::Serialize() const
 {
   auto mCurrentNode = mAstTraversal.GetNextNode();
   while (mCurrentNode != nullptr)
   {
-    int x = (int)mCurrentNode->GetType();
-    cout << tag[x] << " ";
+    cout << "<" << tag[(int)mCurrentNode->GetType()] << " at='line "
+         << mCurrentNode->GetToken()->GetPosition().GetLine() << ", column "
+         << mCurrentNode->GetToken()->GetPosition().GetColumn() << "' Type='"
+         << type[(int)mCurrentNode->GetToken()->GetType()] << "'"
+         << ">" << mCurrentNode->GetToken()->GetWord() << "</" << tag[(int)mCurrentNode->GetType()]
+         << ">" << endl;
+
     mCurrentNode = mAstTraversal.GetNextNode();
   }
 }
